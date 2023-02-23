3c9dfd97094f742f25ab5167e23feb86733f1435
A89-89B <casagabriel3@gmail.com>
2023-02-23 01:46:56 -0300

    Primer componente header

---
+++ src/app/componentes/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  title:string = 'My Task List';
+
+}
+//EN EL VIDEO TIENE:
+//import{ Component, OnInit } from ...(igual)
+//export class HeaderComponent implements OnInit {
+//
+//  constuctor() {}
+//  ngOnInit(): void {
+//  }  
+//}
